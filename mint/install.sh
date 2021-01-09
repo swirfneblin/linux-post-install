2ee1b379d8bbe747505b1db00110d1981aee54ee
Charles Damasceno <swirfneblin@gmail.com>
2021-01-09 12:24:21 -0300

    Update install.sh

--- mint/install.sh
+++ mint/install.sh
@@ -2,13 +2,12 @@
 
 ## SNAP, GIT, TMUX, VIM, KUBECTX, ZSH
 rm /etc/apt/preferences.d/nosnap.pref
-
-apt update && apt install snapd vim tmux build-essential curl file kubectx zsh y
+apt update && apt upgrade && apt install snapd vim tmux build-essential curl file zsh git -y
 
 ## HOMEBREW
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
-## KUBECTL
+## KUBECTL, KUBECTX
 snap install kubectl --classic
 
 ## HELM
@@ -18,17 +17,23 @@ snap install helm --classic
 sudo chsh -s /usr/bin/zsh $USER
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
+cat <<EOF>> ~/.zshrc
+
+EOF
+
+
 ## TEAMVIEWER
 wget https://download.teamviewer.com/download/linux/teamviewer_amd64.deb && apt install -y ./teamviewer_amd64.deb
 
 ## DOTNET
-sudo snap install dotnet-sdk --classic --channel=5.0
-sudo snap alias dotnet-sdk.dotnet dotnet
-sudo snap install dotnet-runtime-50 --classic
-sudo snap alias dotnet-runtime-50.dotnet dotnet
+snap install dotnet-sdk --classic --channel=5.0
+snap install dotnet-runtime-50 --classic
 
 ## LENS
 snap install kontena-lens --classic
+
+## GLOBAL PROTECT 
+## https://drive.google.com/drive/folders/1swpqaTaNFPSNVa5ojAmkzGnXoxCj_QwE
 
 ## STERN
 brew install stern
