fa6e21676bfa33ecc896ca46211c32028b6c0f48
usuario <swirfneblin@gmail.com>
2021-01-09 10:02:54 -0300

    chore: add more some packages

--- mint/install.sh
+++ mint/install.sh
@@ -1,14 +1,37 @@
 #!/bin/sh
 
-#### SNAP, GIT, TMUX, VIM, KUBECTX
-sudo rm /etc/apt/preferences.d/nosnap.pref
+## SNAP, GIT, TMUX, VIM, KUBECTX, ZSH
+rm /etc/apt/preferences.d/nosnap.pref
 
-sudo apt update
-sudo apt install snapd vim tmux build-essential curl file kubectx y
+apt update && apt install snapd vim tmux build-essential curl file kubectx zsh y
 
-#### HOMEBREW
+## HOMEBREW
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
-#### KUBECTL
-sudo snap install kubectl --classic
+## KUBECTL
+snap install kubectl --classic
+
+## HELM
+snap install helm --classic
+
+## ZSH, OH-MY-ZSH
+sudo chsh -s /usr/bin/zsh $USER
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+## TEAMVIEWER
+wget https://download.teamviewer.com/download/linux/teamviewer_amd64.deb && apt install -y ./teamviewer_amd64.deb
+
+## DOTNET
+sudo snap install dotnet-sdk --classic --channel=5.0
+sudo snap alias dotnet-sdk.dotnet dotnet
+sudo snap install dotnet-runtime-50 --classic
+sudo snap alias dotnet-runtime-50.dotnet dotnet
+
+## LENS
+snap install kontena-lens --classic
+
+## STERN
+brew install stern
+
+
 
