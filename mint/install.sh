533ec198c13bbcf6be38c45f27ecebcd8c390b61
Charles Damasceno <swirfneblin@gmail.com>
2021-01-09 12:29:56 -0300

    Update install.sh

--- mint/install.sh
+++ mint/install.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+##CONFIGs
+mkdir -p ~/Documents/github
+
 ## SNAP, GIT, TMUX, VIM, KUBECTX, ZSH
 rm /etc/apt/preferences.d/nosnap.pref
 apt update && apt upgrade && apt install snapd vim tmux build-essential curl file zsh git -y
@@ -16,11 +19,7 @@ snap install helm --classic
 ## ZSH, OH-MY-ZSH
 sudo chsh -s /usr/bin/zsh $USER
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-cat <<EOF>> ~/.zshrc
-
-EOF
-
+curl -k https://raw.githubusercontent.com/swirfneblin/linux-post-install/master/.zshrc >> ~/.zshrc
 
 ## TEAMVIEWER
 wget https://download.teamviewer.com/download/linux/teamviewer_amd64.deb && apt install -y ./teamviewer_amd64.deb
@@ -31,6 +30,9 @@ snap install dotnet-runtime-50 --classic
 
 ## LENS
 snap install kontena-lens --classic
+
+## GH CLI
+brew install gh
 
 ## GLOBAL PROTECT 
 ## https://drive.google.com/drive/folders/1swpqaTaNFPSNVa5ojAmkzGnXoxCj_QwE
